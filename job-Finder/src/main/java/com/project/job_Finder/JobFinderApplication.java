3b250202893fc26683645730aab850236159ec9b
mercadoCODING <raphaelandremercado@gmail.com>
2025-08-01 22:32:04 +0800

    BREAKING CHANGE: first commit

---
+++ job-Finder/src/main/java/com/project/job_Finder/JobFinderApplication.java
@@ -0,0 +1,13 @@
+package com.project.job_Finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFinderApplication.class, args);
+	}
+
+}
---
+++ job-Finder/src/test/java/com/project/job_Finder/JobFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.job_Finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

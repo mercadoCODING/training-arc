21432a32ce4a0e34bfebd76339907b6dedfe573b
mercadoCODING <raphaelandremercado@gmail.com>
2025-08-04 20:38:04 +0800

    add: implemented model,controller,and service to be used as backend materials

---
+++ job-Finder/src/main/java/com/project/job_Finder/service/FetchScrapedDataService.java
@@ -0,0 +1,21 @@
+package com.project.job_Finder.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.project.job_Finder.model.ScrapedData;
+
+
+@Service
+public class FetchScrapedDataService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final String pythonEndpoint = "";
+
+    //apply lambda
+
+    public ScrapedData fetchData(){
+        return restTemplate.getForObject(pythonEndpoint, ScrapedData.class);
+    }
+
+}
